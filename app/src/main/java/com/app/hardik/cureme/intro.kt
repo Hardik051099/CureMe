88173464fb9943d65ff340cf3ed1f174cd7417c9
Hardik05 <hardikarekar05@gmail.com>
2020-01-04 21:11:37 +0530

    Splash Screen done! added logo resources Intro Screen activity added

--- app/src/main/java/com/app/hardik/cureme/intro.kt
+++ app/src/main/java/com/app/hardik/cureme/intro.kt
@@ -3,12 +3,10 @@ package com.app.hardik.cureme
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-
-class MainActivity : AppCompatActivity() {
-
+class intro : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_intro)
     }
 }
