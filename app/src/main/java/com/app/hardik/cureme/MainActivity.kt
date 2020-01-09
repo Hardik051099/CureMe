7f5c94dc460135cc072a57c8e4ab06e629eec954
Hardik05 <hardikarekar05@gmail.com>
2020-01-09 15:13:19 +0530

    Splash Screen done! added logo resources Intro Screen activity added Login screen added

--- app/src/main/java/com/app/hardik/cureme/MainActivity.kt
+++ app/src/main/java/com/app/hardik/cureme/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.app.hardik.cureme
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +12,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //splash screen to display it for 2 seconds
+        val splash = Intent(this@MainActivity,intro::class.java)
+        Handler().postDelayed({
+            startActivity(splash)
+            finish()
+        },2000 )//equal to 2 seconds
     }
 }
--- app/src/main/java/com/app/hardik/cureme/intro.kt
+++ app/src/main/java/com/app/hardik/cureme/intro.kt
@@ -2,11 +2,25 @@ package com.app.hardik.cureme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
 
 class intro : AppCompatActivity() {
+    //globally declaring
+lateinit var login : Button
+lateinit var signup : Button
+    fun login (view: View){
 
+    }
+    fun signup (view: View) {
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+        //assigning ids
+        login = findViewById(R.id.login)
+        signup = findViewById(R.id.login)
     }
 }
